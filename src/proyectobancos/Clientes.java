8ae1f7429ea80356c0a50587e146d0fa1b577058
Milleniumruiz <milleniumruiz@gmail.com>
2014-10-05 01:26:46 -0600

    correo incompleto

---
+++ src/proyectobancos/Clientes.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package proyectobancos;
+
+/**
+ *
+ * @author Miller
+ */
+public class Clientes {
+    
+    public String nombre;
+    public String correo;
+    public String tipoPrioridad;
+    public String fecha;
+    public String hora;
+    
+    public Clientes(String nombre, String tipoPrioridad, String correo, String fecha, String hora){
+        this.nombre = nombre;
+        this.tipoPrioridad = tipoPrioridad;
+        this.correo = correo;
+        this.fecha = fecha;
+        this.hora = hora;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public String getTipoPrioridad(){
+        return tipoPrioridad;
+    }
+    
+    public String getCorreo(){
+        return correo;
+    }
+    
+    public void setTipoPrioridad(String prioridad){
+        this.tipoPrioridad = prioridad;
+    }
+    
+    public String getFecha(){
+        return fecha;
+    }
+    
+    public String getHora(){
+        return hora;
+    }
+}
+
---
+++ src/proyectobancos/CorreoClientes.java
@@ -0,0 +1,141 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package proyectobancos;
+
+/**
+ *
+ * @author Miller
+ */
+import java.util.Properties;
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+public class CorreoClientes {
+    String usuarioCorreo;
+    String password;
+    String rutaArchivo;
+    String nombreArchivo;
+    String asunto;
+    String destinatario;
+    
+    public CorreoClientes(String usuarioCorreo, String password, String rutaArchivo, String nombreArchivo, String asunto){
+        this.usuarioCorreo = usuarioCorreo;
+        this.password = password;
+        this.rutaArchivo = rutaArchivo;
+        this.nombreArchivo = nombreArchivo;
+        this.asunto = asunto;
+    }   
+    //Constructor para la confirmacion de llegada al banco
+    public boolean sendMail(String cliente, String destinatario, String Fecha, String Hora){
+        this.destinatario = destinatario;
+        try
+        {
+            Properties props = new Properties();
+            props.put("mail.smtp.host", "smtp.gmail.com");
+            props.setProperty("mail.smtp.starttls.enable", "true");
+            props.setProperty("mail.smtp.port", "587");
+            props.setProperty("mail.smtp.user", usuarioCorreo);
+            props.setProperty("mail.smtp.auth", "true");
+
+            Session session = Session.getDefaultInstance(props, null);
+            BodyPart texto = new MimeBodyPart();
+            texto.setText( "Estimado cliente "+cliente+" este es un correo de confirmacion de su llegada al banco el "//Mensaje por defecto para la confirmacion de la llegada del cliente
+            + Fecha + " a las " + Hora);
+
+            BodyPart adjunto = new MimeBodyPart();
+            if (!rutaArchivo.equals("")){
+                 adjunto.setDataHandler(
+                    new DataHandler(new FileDataSource(rutaArchivo)));
+                adjunto.setFileName(nombreArchivo);                
+            }
+
+            MimeMultipart multiParte = new MimeMultipart();
+            multiParte.addBodyPart(texto);
+            if (!rutaArchivo.equals("")){
+                multiParte.addBodyPart(adjunto);
+            }
+
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(usuarioCorreo));
+            message.addRecipient(
+                Message.RecipientType.TO,
+                new InternetAddress(destinatario));
+                message.setSubject(asunto);
+            message.setContent(multiParte);
+
+            Transport t = session.getTransport("smtp");
+            t.connect(usuarioCorreo, password);
+            t.sendMessage(message, message.getAllRecipients());
+            t.close();
+            return true;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return false;
+        }        
+    }
+    //constructor del correo para la asignacion de la caja
+    public boolean sendMail(String caja, String cliente, String destinatario){
+        this.destinatario = destinatario;
+        try
+        {
+            Properties props = new Properties();
+            props.put("mail.smtp.host", "smtp.gmail.com");
+            props.setProperty("mail.smtp.starttls.enable", "true");
+            props.setProperty("mail.smtp.port", "587");
+            props.setProperty("mail.smtp.user", usuarioCorreo);
+            props.setProperty("mail.smtp.auth", "true");
+
+            Session session = Session.getDefaultInstance(props, null);
+            BodyPart texto = new MimeBodyPart();
+            texto.setText( "Estimado cliente "+cliente+" por favor pase a la " + caja);//Mensaje por defecto para que el cliente pase a la caja asignada
+
+            BodyPart adjunto = new MimeBodyPart();
+            if (!rutaArchivo.equals("")){
+                 adjunto.setDataHandler(
+                    new DataHandler(new FileDataSource(rutaArchivo)));
+                adjunto.setFileName(nombreArchivo);                
+            }
+
+            MimeMultipart multiParte = new MimeMultipart();
+            multiParte.addBodyPart(texto);
+            if (!rutaArchivo.equals("")){
+                multiParte.addBodyPart(adjunto);
+            }
+
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(usuarioCorreo));
+            message.addRecipient(
+                Message.RecipientType.TO,
+                new InternetAddress(destinatario));
+                message.setSubject(asunto);
+            message.setContent(multiParte);
+
+            Transport t = session.getTransport("smtp");
+            t.connect(usuarioCorreo, password);
+            t.sendMessage(message, message.getAllRecipients());
+            t.close();
+            return true;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return false;
+        }        
+    }
+    
+   
+}
