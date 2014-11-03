48fef5eaf0d342a7faaf4775e99bed6eb8465036
Joseph <j-vv01@hotmail.com>
2014-11-02 21:56:32 -0600

    Ajuste Final para ejecución

--- src/proyectobancos/Administradores/estructuras/colas/QueuePropia.java
+++ src/proyectobancos/Administradores/estructuras/colas/QueuePropia.java
@@ -17,7 +17,6 @@ public interface QueuePropia<AnyType> extends Collection<AnyType>
      * Returns but does not EliminaNodo the item at the "front"
  of the queue.
      * @return the front item of null if the queue is empty.
-     * @throws NoSuchElementException if the queue is empty.
      */
     AnyType element( );
     
@@ -25,7 +24,6 @@ public interface QueuePropia<AnyType> extends Collection<AnyType>
      * Returns and removes the item at the "front"
      * of the queue.
      * @return the front item.
-     * @throws NoSuchElementException if the queue is empty.
      */
     AnyType EliminaNodo( );
 }
